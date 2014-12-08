7a4c3007a23aaa6c1f5362a373ab70d486c33861
Alexander Schnapp <alex.schnapp@googlemail.com>
2014-12-09 00:21:19 +0100

    finished 7

--- uebungen/7/N-body_highN.cc
+++ uebungen/7/N-body_highN.cc
@@ -5,108 +5,135 @@
 #include <time.h>
 using namespace std;
 
-
-
-double cubic_abstand(double x1, double x2, double x3, double y1, double y2,double y3)
+void print_pos(int *N,double ***x,double *t)
 {
-    double abstand=sqrt((x1-y1)*(x1-y1)+(x2-y2)*(x2-y2)+(x3-y3)*(x3-y3));
-    return abstand*abstand*abstand;
+     ofstream out;
+       char filename[20];
+       snprintf (filename, sizeof filename, "data/2e/data_t_%f_.dat", *t);
+    out.open(filename);
+ //ausgabe der werte
+    for(int i=0;i<*N;i++)
+    {for(int j=0;j<3;j++)
+        {out  <<*x[i][j]<<"\t";}
+    out <<endl;}
+    out.close();
+}
+
+
+
+void accelarations(int *N, double ***x,double ***a,double **m)
+{
+    //Initialisieren
+    for(int i=0;i<*N;i++)
+    {for(int j=0;j<3;j++){(*a)[i]=0;}}
+
+    //schleife über alle teilchen paare
+    for(int i=0;i<*N;i++)
+    {
+        for(int j=i+1;j<*N;j++)
+        {
+
+                 double dx=(*x)[j][0]-(*x)[i][0];
+                double dy=(*x)[j][1]-(*x)[i][1];
+                double dz=(*x)[j][2]-(*x)[i][2];
+
+                double diff= dx*dx+dy*dy+dz*dz;
+                double kraft=-6.673e-11*(*m)[i]*(*m)[j]/diff;
+
+
+                (*a)[i][0]+=kraft*dx/(*m)[i];
+                (*a)[i][1]+=kraft*dy/(*m)[i];
+                (*a)[i][2]+=kraft*dz/(*m)[i];
+
+                //kraft und gegenkraft, da keine interaktion doppelt gezählt wird
+                (*a)[j][0]-=kraft*dx/(*m)[j];
+                (*a)[j][1]-=kraft*dy/(*m)[j];
+                (*a)[j][2]-=kraft*dz/(*m)[j];
+
+        }
+    }
+
+}
+
+void pos_update(int *N,double dt,double (***x),double(***a))
+{
+     //Teilchen
+    for(int i=0;i<*N;i++)
+    {   //Richtungen
+        for(int j=0;j<3;j++)
+        {
+            (*x)[i][j]+=(*a)[i][j]*(dt)*(dt);
+        }
+    }
 }
 
 int main (int argc, char** argv)
 {
-	N    = atoi(argv[1]);
+int	N    = atoi(argv[1]);
  //Fixing the  constants
 const double dt=0.001;
-const double tmax=1;
+const int steps=1000;
 
- //MAssen
-double m[N];
-for(int i=0;i<N;i++) m[i]=1;
-
+//time varialbe
 double t=0;
 
-//arrays for position and velocities
-double x[N][3]={0};
-double xnew[N][3]={0};
-double v[N][3]={0};
-double vhalf=0;
-double vnew[N][3]={0};
+//arrays for position and beschleunigung und massen
+    double **x;
+    double **a;
+    double *m;
+    x=(double **) malloc(N*sizeof(double*));
+    for(int i=0;i<3;i++) x[i]= (double *) malloc(3*sizeof(double));
+    a=(double **) malloc(N*sizeof(double*));
+    for(int i=0;i<3;i++) a[i]= (double *) malloc(3*sizeof(double));
+    m=(double *) malloc(N*sizeof(double));
 
-//zufallszahlen generator initialisieren
-srand((unsigned)time(NULL));
+    //zufallszahlen generator initialisieren
+    srand((unsigned)time(NULL));
 
-//anfangsbedingen
- //Orte
+    //anfangsbedingen
+    //Orte
     for (int i=0;i<N;i++)
-    {
-            x[i][0]=rand()*1./RAND_MAX;
-            x[i][1]=rand()*1./RAND_MAX;
-            x[i][2]=rand()*1./RAND_MAX;
-    if (x[i][0]*x[i][0]+x[i][1]*x[i][1]+x[i][2]*x[i][2]>1)
-       { i--;} //rejection
-    }
-
-
-//step counter
-int s=0;
-//time loop for integration
-while(t<tmax)
-{
-    if(s%100==0)
-    {
-       ofstream out;
-       char filename[20];
-       snprintf (filename, sizeof filename, "data/2e/data_t_%f_.dat", t);
-    out.open(filename);
- //ausgabe der werte
-    for(int i=0;i<N;i++)
     {for(int j=0;j<3;j++)
-        {out  <<"\t" <<x[i][j];}
-    out <<endl;}
-    out.close();
-    }
-    //Berechnung der neuen Geschwindigkeiten und Positionen (Leapfrog algorithm)
-
-    //alle Richtungen
-    for(int j=0;j<3;j++)
-    {
-        //alle Teilchen
-        for(int i=0;i<N;i++)
-        {
-            vhalf=v[i][j];
-            //alle anderen teilhen
-            for(int k=0;k<N;k++)
-            {if(k!=i){
-                vhalf+=m[i]*m[k]*(x[k][j]-x[i][j])/cubic_abstand(x[k][0],x[k][1],x[k][2],x[i][0],x[i][1],x[i][2])*dt/2;
-            }}
-
-            xnew[i][j]=x[i][j]+vhalf*dt;
-            vnew[i][j]=vhalf;
-
-             //alle anderen teilhen
-            for(int k=0;k<N;k++)
-            {if(k!=i){
-                vnew[i][j]+=m[i]*m[k]*(xnew[k][j]-xnew[i][j])/cubic_abstand(x[k][0],x[k][1],x[k][2],x[i][0],x[i][1],x[i][2])*dt/2;
-            }}
-        }
+            x[i][j]=rand()*1./RAND_MAX;
     }
 
-    // Umspeichurung neue in alte Koordinaten
-
-    for(int i=0;i<N;i++)
+    //accelarations
+    for (int i=0;i<N;i++)
     {for(int j=0;j<3;j++)
-        {
-            x[i][j]=xnew[i][j];
-            v[i][j]=vnew[i][j];
-        }
+            a[i][j]=0;
     }
 
- t+=dt;
- s++;
+    //Massen
+   for (int i=0;i<N;i++)
+    {
+            m[i]=rand();
+    }
+
+
+    //step counter
+    int s=0;
+    //time loop for integration
+    while(s<steps)
+    {
+        //Ausgabe
+        if(s%100==0)
+        { print_pos(&N,&x,&t); }
+
+
+        //Berechnung der neuen Beschleunigungen
+        accelarations(&N,&x,&a,&m);
+
+       //Berechnung der nuene Orte
+       pos_update(&N,dt,&x,&a);
+
+
+    t+=dt;
+    s++;
 }
 
-
+free(x);
+free(a);
+free(m);
 
 
 }
